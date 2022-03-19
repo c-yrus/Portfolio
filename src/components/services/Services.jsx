f9e8e692dc69a5cacbcdf0918385d058b0b98f29
Hazem Abdennadher <Hazem0Abdennadher@gmail.com>
2022-03-19 23:07:53 +0100

    experience + services

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,66 @@
 import "./services.css"
+import {BsCheck2} from "react-icons/bs"
 const Services = () => {
     return ( 
         <section id="services">
-            services
+            <h5>What I Offer</h5>
+            <h2>Services</h2>
+            <div className="container services__container">
+                <article className="service">
+                    <div className="service__head">
+                        <h3>Web Development</h3>
+                    </div>
+                    <ul className="service__list">
+                        <li>
+                            <BsCheck2 className="service__list-icon"/>
+                            <p>Lorem ipsum dolor sit amet.</p>
+                        </li>
+                        <li>
+                            <BsCheck2 className="service__list-icon"/>
+                            <p>Lorem ipsum dolor sit amet.</p>
+                        </li>
+                        <li>
+                            <BsCheck2 className="service__list-icon"/>
+                            <p>Lorem ipsum dolor sit amet.</p>
+                        </li>
+                        <li>
+                            <BsCheck2 className="service__list-icon"/>
+                            <p>Lorem ipsum dolor sit amet.</p>
+                        </li>
+                        <li>
+                            <BsCheck2 className="service__list-icon"/>
+                            <p>Lorem ipsum dolor sit amet.</p>
+                        </li>
+                    </ul>
+                </article>
+                <article className="service">
+                    <div className="service__head">
+                        <h3>Web Development</h3>
+                    </div>
+                    <ul className="service__list">
+                        <li>
+                            <BsCheck2 className="service__list-icon"/>
+                            <p>Lorem ipsum dolor sit amet.</p>
+                        </li>
+                        <li>
+                            <BsCheck2 className="service__list-icon"/>
+                            <p>Lorem ipsum dolor sit amet.</p>
+                        </li>
+                        <li>
+                            <BsCheck2 className="service__list-icon"/>
+                            <p>Lorem ipsum dolor sit amet.</p>
+                        </li>
+                        <li>
+                            <BsCheck2 className="service__list-icon"/>
+                            <p>Lorem ipsum dolor sit amet.</p>
+                        </li>
+                        <li>
+                            <BsCheck2 className="service__list-icon"/>
+                            <p>Lorem ipsum dolor sit amet.</p>
+                        </li>
+                    </ul>
+                </article>
+            </div>
         </section>
     );
 }
